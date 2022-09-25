5973c1eef07b7314d0e3f53621c4d1a2262a5b4b
Humaid <humaidfarhan@gmail.com>
2022-09-25 13:12:09 +0400

    Final project

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post_detail/<int:post_id>', views.post_detail, name='post_detail'),
+    path('add_post/', views.add_post, name='add_post'),
+    path('delete_post/<int:post_id>', views.delete_post, name='delete_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    context = {'post': post}
+    return render(request, 'blog/post_detail.html', context)
+
+def add_post(request):
+    if request.method != 'POST':
+        form = PostForm()
+    else:
+            form = PostForm(data=request.POST)
+            if form.is_valid():
+                new_post = form.save(commit=False)
+                new_post.author = request.user
+                new_post.save()
+                return redirect('blog:post_list')
+    context = {'form': form}
+    return render(request, 'blog/add_post.html', context)
+
+def delete_post(reuqest, post_id):
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return redirect('blog:post_list')
