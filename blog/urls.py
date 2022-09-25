90d38e2a7360fe9990031b1e7c88ac494de9600c
Humaid <humaidfarhan@gmail.com>
2022-09-25 13:37:45 +0400

    Final Project2

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('post_detail/<int:post_id>', views.post_detail, name='post_detail'),
     path('add_post/', views.add_post, name='add_post'),
     path('delete_post/<int:post_id>', views.delete_post, name='delete_post'),
+    path('edit_post/<int:post_id>', views.edit_post, name='edit_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django.shortcuts import redirect, render
 from .models import Post
 from .forms import PostForm
@@ -29,3 +30,29 @@ def delete_post(reuqest, post_id):
     post = Post.objects.get(id=post_id)
     post.delete()
     return redirect('blog:post_list')
+
+def add_post(request):
+    if request.method != 'POST':
+        form = PostForm()
+    else:
+            form = PostForm(data=request.POST)
+            if form.is_valid():
+                new_post = form.save(commit=False)
+                new_post.author = request.user
+                new_post.save()
+                return redirect('blog:post_list')
+    context = {'form': form}
+    return render(request, 'blog/add_post.html', context)
+
+def edit_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method != 'POST':
+        form = PostForm(instance=post)
+    else:
+        form = PostForm(instance=post, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:post_detail', post_id)
+    context = {'form': form, 'post': post}
+    return render(request, 'blog/edit_post.html', context)
+
